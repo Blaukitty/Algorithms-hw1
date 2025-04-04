4ef4682edc22e84c2f8615fa447ab6afd2c6e0d8
Blaukitty <st132496@student.spbu.ru>
2025-04-04 10:20:43 +0300

    header

---
+++ decoder_h.h
@@ -0,0 +1,12 @@
+﻿// decoder_alg.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+
+#pragma once
+
+#include <iostream>
+#include <string>
+
+// Кодирует данные из входного потока в строку ASCII85
+std::string encode(std::istream& in);
+
+// Декодирует строку ASCII85 и записывает результат в выходной поток
+void decode(const std::string& encoded, std::ostream& out);
