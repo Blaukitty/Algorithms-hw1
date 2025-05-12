ac30e818d0016fed55322000387b9fa848860035
Rodionova Ksenia <st132496@student.spbu.ru>
2025-05-12 12:34:13 +0300

    Update test_driver.py

--- test_driver.py
+++ test_driver.py
@@ -1,23 +1,16 @@
-import base64
-import subprocess
-import os
+import base64, subprocess, os, pathlib
 
-Decoder = "./decoder_alg"
+BIN = "./ascii85_cli"   # ← имя итогового бинаря после сборки
 def correct_data_test():
-    data = os.urandom(50)
-    encoded = base64.a85encode(data)
-    
-    with open("input.txt", "wb") as f:
-        f.write(encoded)
-        
-    result = subprocess.run([Decoder], stdin=open("input.txt", "rb"), stdout=subprocess.PIPE)
-    print("correct data: exit code = ", result.returncode)
-    
+    raw = os.urandom(50)
+    ascii85 = base64.a85encode(raw)
+    r = subprocess.run([BIN], input=ascii85, stdout=subprocess.PIPE)
+    print("correct exit code =", r.returncode)
+
 def invalid_data_test():
-    with open("input.txt", "wb") as f:
-        f.write(b"!!!***invalid***data***!!!")
-    result = subpricess.run([Decoder], stdin=open("input.txt", "rb"), stdout=subprocess.PIPE)
-    print("invalid data: exit code = ", result.returncode)
-    
+    bad = b"!!! invalid***data !!!~>"      # добавили ~>
+    r   = subprocess.run([BIN], input=bad, stdout=subprocess.PIPE)
+    print("invalid exit code =", r.returncode)
+
 correct_data_test()
 invalid_data_test()
